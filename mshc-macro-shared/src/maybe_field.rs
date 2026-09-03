4620a4315932d4ebee26691a356e3941cd6e1f5a
Markku Sukanen <markku.sukanen@gmail.com>
2026-09-04 01:33:24 +0300

    dual fielder...

--- mshc-macro-shared/src/maybe_field.rs
+++ mshc-macro-shared/src/maybe_field.rs
@@ -12,3 +12,18 @@ macro_rules! maybe_field {
         })  .map(|f| f.ident.as_ref().unwrap())
     };
 }
+
+#[macro_export]
+macro_rules! maybe_field2 {
+    (named $data:ident, $field:expr) => {
+        $data.named.iter().find(|f| {
+            f.ident.as_ref().map_or(false, |i| i == $field)
+        })  .map(|f| (f, f.ident.as_ref().unwrap()))
+    };
+
+    ($data:ident, $field:expr) => {
+        $data.fields.iter().find(|f| {
+            f.ident.as_ref().map_or(false, |i| i == $field)
+        })  .map(|f| (f, f.ident.as_ref().unwrap()))
+    };
+}
--- mshc-macro-shared/src/req_field.rs
+++ mshc-macro-shared/src/req_field.rs
@@ -10,3 +10,16 @@ macro_rules! req_field {
             .expect(&format!("No '{}' field found in struct layout", $field))
     };
 }
+
+#[macro_export]
+macro_rules! req_field2 {
+    (named $data:ident, $field:expr) => {
+        $crate::maybe_field2!(named $data, $field)
+            .expect(&format!("No '{}' field found in struct layout", $field))
+    };
+
+    ($data:ident, $field:literal) => {
+        $crate::maybe_field2!($data, $field)
+            .expect(&format!("No '{}' field found in struct layout", $field))
+    };
+}
